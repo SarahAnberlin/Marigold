a40420c39bddc48df07d45caa761078ba7dd1c78
SarahAnberlin <sarahanberlin@outlook.com>
2025-03-13 19:10:37 +0800

    '?'

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import os
+
 from diffusers import MarigoldDepthPipeline, DDPMScheduler
 from PIL import Image
 import torchvision
@@ -41,6 +43,7 @@ text_inputs = tokenizer(
 )
 text_input_ids = text_inputs.input_ids.to(device)
 save_root = 'output'
+os.makedirs(save_root, exist_ok=True)
 with torch.no_grad():
     empty_text_embedding = text_encoder(text_input_ids)[0]
     print(f"Shape of empty_text_embedding: {empty_text_embedding.shape}")
@@ -62,14 +65,14 @@ with torch.no_grad():
     rgb_noise_scheduler.set_timesteps(rgb_scheduler_timestep, device=device)
     timesteps = torch.tensor([i for i in range(rgb_scheduler_timestep)]).to(device)
 
-    for t in timesteps:
+    for rgb_t in timesteps:
         noise = torch.randn(
             latent.shape,
             device=device,
             generator=generator,
         )  # [B, 4, h, w]
         noisy_latent = rgb_noise_scheduler.add_noise(
-            latent, noise, t
+            latent, noise, rgb_t,
         )  # [B, 4, h, w]
 
         batch_image_latent = noisy_latent
@@ -79,13 +82,13 @@ with torch.no_grad():
 
         time_step_to_denoise = 50
         scheduler.set_timesteps(time_step_to_denoise, device=device)
-        for t in scheduler.timesteps:
+        for depth_denoise_t in scheduler.timesteps:
             batch_latent = torch.cat([batch_image_latent, batch_pred_latent], dim=1)  # [B,8,h,w]
             print(f"Shape of batch_latent: {batch_latent.shape}")
-            noise = unet(batch_latent, t, encoder_hidden_states=text, return_dict=False)[0]  # [B,4,h,w]
+            noise = unet(batch_latent, depth_denoise_t, encoder_hidden_states=text, return_dict=False)[0]  # [B,4,h,w]
             print(f"Shape of noise: {noise.shape}")
             batch_pred_latent = scheduler.step(
-                noise, t, batch_pred_latent, generator=generator
+                noise, depth_denoise_t, batch_pred_latent, generator=generator
             ).prev_sample  # [B,4,h,w]
             print(f"Shape of batch_pred_latent: {batch_pred_latent.shape}")
 
@@ -95,5 +98,5 @@ with torch.no_grad():
         depth = torch.clip(depth, -1, 1)
         depth = (depth + 1) / 2
 
-        save_image(depth, f"{save_root}/depth_{t}.png")
-        save_image(rgb, f"{save_root}/rgb_{t}.png")
+        save_image(depth, f"{save_root}/depth_{rgb_t}.png")
+        save_image(rgb, f"{save_root}/rgb_{rgb_t}.png")
