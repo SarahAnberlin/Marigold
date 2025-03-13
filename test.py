a15851ad5a663f163e167ed3839e2bd286e39ad2
SarahAnberlin <sarahanberlin@outlook.com>
2025-03-13 19:00:34 +0800

    '?'

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from diffusers import MarigoldDepthPipeline
+from diffusers import MarigoldDepthPipeline, DDPMScheduler
 from PIL import Image
 import torchvision
 from diffusers.utils.torch_utils import randn_tensor
@@ -28,6 +28,7 @@ transform = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Resize((512, 512)),
 ])
+
 image = transform(image).unsqueeze(0).to(device)
 
 prompt = ''
@@ -39,6 +40,7 @@ text_inputs = tokenizer(
     return_tensors="pt",
 )
 text_input_ids = text_inputs.input_ids.to(device)
+save_root = 'output'
 with torch.no_grad():
     empty_text_embedding = text_encoder(text_input_ids)[0]
     print(f"Shape of empty_text_embedding: {empty_text_embedding.shape}")
@@ -55,26 +57,43 @@ with torch.no_grad():
         device=latent.device,
         dtype=latent.dtype,
     )
+    rgb_noise_scheduler = DDPMScheduler.from_pretrained('stable-diffusion-2', device=device)
+    rgb_scheduler_timestep = 50
+    rgb_noise_scheduler.set_timesteps(rgb_scheduler_timestep, device=device)
+    timesteps = torch.tensor([i for i in range(rgb_scheduler_timestep)]).to(device)
 
-    batch_image_latent = latent
-    batch_pred_latent = pred_latent
-    effective_batch_size = batch_image_latent.shape[0]
-    text = empty_text_embedding[:effective_batch_size]  # [B,2,1024]
+    for t in timesteps:
+        noise = torch.randn(
+            latent.shape,
+            device=device,
+            generator=generator,
+        )  # [B, 4, h, w]
+        noisy_latent = rgb_noise_scheduler.add_noise(
+            latent, noise, t
+        )  # [B, 4, h, w]
 
-    time_step_to_denoise = 50
-    scheduler.set_timesteps(time_step_to_denoise, device=device)
-    for t in scheduler.timesteps:
-        batch_latent = torch.cat([batch_image_latent, batch_pred_latent], dim=1)  # [B,8,h,w]
-        print(f"Shape of batch_latent: {batch_latent.shape}")
-        noise = unet(batch_latent, t, encoder_hidden_states=text, return_dict=False)[0]  # [B,4,h,w]
-        print(f"Shape of noise: {noise.shape}")
-        batch_pred_latent = scheduler.step(
-            noise, t, batch_pred_latent, generator=generator
-        ).prev_sample  # [B,4,h,w]
-        print(f"Shape of batch_pred_latent: {batch_pred_latent.shape}")
+        batch_image_latent = noisy_latent
+        batch_pred_latent = pred_latent
+        effective_batch_size = batch_image_latent.shape[0]
+        text = empty_text_embedding[:effective_batch_size]  # [B,2,1024]
 
-    depth = vae.decode(batch_pred_latent / vae.config.scaling_factor, return_dict=False)[0]
-    depth = depth.mean(dim=1, keepdim=True)
-    depth = torch.clip(depth, -1, 1)
-    depth = (depth + 1) / 2
-    save_image(depth, 'depth.jpg')
+        time_step_to_denoise = 50
+        scheduler.set_timesteps(time_step_to_denoise, device=device)
+        for t in scheduler.timesteps:
+            batch_latent = torch.cat([batch_image_latent, batch_pred_latent], dim=1)  # [B,8,h,w]
+            print(f"Shape of batch_latent: {batch_latent.shape}")
+            noise = unet(batch_latent, t, encoder_hidden_states=text, return_dict=False)[0]  # [B,4,h,w]
+            print(f"Shape of noise: {noise.shape}")
+            batch_pred_latent = scheduler.step(
+                noise, t, batch_pred_latent, generator=generator
+            ).prev_sample  # [B,4,h,w]
+            print(f"Shape of batch_pred_latent: {batch_pred_latent.shape}")
+
+        depth = vae.decode(batch_pred_latent / vae.config.scaling_factor, return_dict=False)[0]
+        rgb = vae.decode(noisy_latent / vae.config.scaling_factor, return_dict=False)[0]
+        depth = depth.mean(dim=1, keepdim=True)
+        depth = torch.clip(depth, -1, 1)
+        depth = (depth + 1) / 2
+
+        save_image(depth, f"{save_root}/depth_{t}.png")
+        save_image(rgb, f"{save_root}/rgb_{t}.png")
