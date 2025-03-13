0fa5e34831550b931e26aba5932d86b15f9874b5
SarahAnberlin <sarahanberlin@outlook.com>
2025-03-13 19:06:51 +0800

    '?'

--- test.py
+++ test.py
@@ -57,7 +57,7 @@ with torch.no_grad():
         device=latent.device,
         dtype=latent.dtype,
     )
-    rgb_noise_scheduler = DDPMScheduler.from_pretrained('stable-diffusion-2', device=device)
+    rgb_noise_scheduler = DDPMScheduler.from_pretrained('google/ddpm-ema-celebahq-256', device=device)
     rgb_scheduler_timestep = 50
     rgb_noise_scheduler.set_timesteps(rgb_scheduler_timestep, device=device)
     timesteps = torch.tensor([i for i in range(rgb_scheduler_timestep)]).to(device)
